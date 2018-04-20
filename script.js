74c9b1db979dd52da9051d939443427b530cb236
158Alfetta <32899555+158Alfetta@users.noreply.github.com>
2018-04-20 11:09:35 +0700

    abc

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var $win = $(window);
 $win.on('scroll', function () {
   // This event will be fired every time the browser scrolls
   var topPixels = $win.scrollTop();
+  console.log(topPixels);
   if(topPixels > 1){
     $('.nav').css('background-color', 'rgba(40, 46, 52, ' + topPixels/700 + ')');
-    $('.nav a').css('color', '#fff');
   }
 });
