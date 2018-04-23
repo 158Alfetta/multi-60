a21524aadda73570a2011a5140423e091bc2b617
158Alfetta <32899555+158Alfetta@users.noreply.github.com>
2018-04-23 16:30:38 +0700

    modal
    Add modal.

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var $win = $(window);
 
 $win.on('scroll', function () {
-  // This event will be fired every time the browser scrolls
-  var topPixels = $win.scrollTop();
-  console.log(topPixels);
-  if(topPixels/700 > 0.33){
-  	$('.nav').css('background-color', 'rgba(40, 46, 52, ' + topPixels/700 + ')');
-  	$('.cur').css('background-color', 'rgba(100, 100, 100, ' + topPixels/700 + ')');
-  }
-  else{
-  	$('.cur').css('background-color', 'rgba(30, 30, 30, 0.33)');
-  }
+// This event will be fired every time the browser scrolls
+var topPixels = $win.scrollTop();
+console.log(topPixels);
+if(topPixels/700 > 0.33){
+  $('.nav').css('background-color', 'rgba(40, 46, 52, ' + topPixels/700 + ')');
+  $('.cur').css('background-color', 'rgba(100, 100, 100, ' + topPixels/700 + ')');
+}
+else{
+  $('.cur').css('background-color', 'rgba(30, 30, 30, 0.33)');
+}
 });
+
+// window.onclick = function() {
+//   $('.modal').css('display', 'none');
+// };
+
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+function closeModal() {
+  $('.modal').css('display', 'none');
+}
