ce0290c65429f6545bcf47215f4250c23745618c
fpiyapol <fpiyapol@users.noreply.github.com>
2018-04-28 10:10:56 +0700

    .

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var $win = $(window);
 
 $( document ).ready(function() {
-     $('.modal').fadeIn(600, "linear");
+	$('.modal').fadeIn(600, "linear");
+
 });
 
 $win.on('scroll', function () {
@@ -9,21 +10,18 @@ $win.on('scroll', function () {
 var topPixels = $win.scrollTop();
 console.log(topPixels);
 if(topPixels/700 > 0.33){
-  $('.nav').css('background-color', 'rgba(40, 46, 52, ' + topPixels/700 + ')');
-  $('.cur').css('background-color', 'rgba(100, 100, 100, ' + topPixels/700 + ')');
+	console.log("????");
+	$('.nav').css('background-color', 'rgba(40, 46, 52, ' + topPixels/700 + ')');
+	$('.cur').css('background-color', 'rgba(100, 100, 100, ' + topPixels/700 + ')');
 }
 else{
-  $('.cur').css('background-color', 'rgba(30, 30, 30, 0.33)');
+	$('.cur').css('background-color', 'rgba(30, 30, 30, 0.33)');
 }
 });
 
 function closeModal() {
 	$('#vdo')[0].pause();
-  $('.modal').fadeOut(200, "linear");
+	$('.modal').fadeOut(200, "linear");
   // $('.modal').css('display', 'none');
 }
 
-// $('.con').hover(function(){
-//   console.log("hover");
-//   $('.text').append("อ่านเพิ่มเติม");
-// });
